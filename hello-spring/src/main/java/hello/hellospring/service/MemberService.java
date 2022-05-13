de14e4f539c3c0a8e4114a26795bfae80076a31f
Parkshouzhen <shouzhenn@gmail.com>
2022-05-13 10:03:41 +0900

    220513 회원 서비스 개발

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hellospring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+	// ---------- 회원가입 -----------
+
+	public Long join(Member member) {
+		// 같은 이름이 있는 중복 회원 X
+		ValidateDuplicateMember(member); // 중복 회원 검증
+
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void ValidateDuplicateMember(Member member) {
+		// Optional<Member> result = memberRepository.findByName(member.getName());
+
+		memberRepository.findByName(member.getName()).ifPresent(m -> { // ifPresent 값이 있으면, Optional이기 때문에 가능
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+
+	// --------------전체 회원 조회-----------------
+	public List<Member> findMember() {
+		return memberRepository.findAll();
+	}
+
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+}
